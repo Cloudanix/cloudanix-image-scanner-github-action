3e9a672e4dff31cc9364ac046d025ca172c4aba0
Purusottam <mpurusottamc@gmail.com>
2024-11-29 08:14:16 -0800

    get rid of unnecessary params

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Container image that scans code
-FROM cloudanix/image-scanner
+FROM cloudanix/container-image-scanner:v0.0.12-dev
 
 # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,35 +1,23 @@
 #!/bin/bash
 set -e
-while getopts "a:b:c:d:e:f:g:h:i:" o; do
+while getopts "a:b:c:d:" o; do
   case "${o}" in
     a)
-      export type="${OPTARG-"image"}"
-    ;;
-    b)
       export imageRef="${OPTARG}"
     ;;
-    c)
-      export format="${OPTARG-"json"}"
-    ;;
-    d)
-      export scanType="${OPTARG-"vuln"}"
-    ;;
-    e)
-      export timeout="${OPTARG}"
-    ;;
-    f)
-      export apiEndpoint="${OPTARG}"
-    ;;
-    g)
+    b)
       export authZToken="${OPTARG}"
     ;;
-    h)
+    c)
       export identifier="${OPTARG}"
+    ;;
+    d)
+      export debugMode="${OPTARG:false}"
     ;;
   esac
 done
 
-image-scanner $type $imageRef --no-progress --db-repository "ghcr.io/aquasecurity/trivy-db,docker.io/aquasec/trivy-db" --java-db-repository "ghcr.io/aquasecurity/trivy-java-db,docker.io/aquasec/trivy-java-db" --format $format --scanners $scanType --api-endpoint $apiEndpoint --authz-token $authZToken --identifier $identifier
+imagescanner --imageToScan $imageRef --authZToken $authZToken --identifier $identifier --debugMode $debugMode
 
 returnCode=$?
 
